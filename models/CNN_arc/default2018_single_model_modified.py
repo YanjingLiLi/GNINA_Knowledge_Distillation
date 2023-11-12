42ddd4085d15d38dcb9915f093f0e73a7ec9d87c
Yanjing Li <yanjing@pitt.edu>
2023-11-11 19:59:41 -0500

    add files

---
+++ models/CNN_arc/default2018_single_model_modified.py
@@ -0,0 +1,71 @@
+import torch.nn as nn
+import torch.nn.functional as F
+import numpy as np
+import torch
+from torch import tensor
+
+class View(nn.Module):
+        def __init__(self,shape):
+                super(View, self).__init__()
+                self.shape = shape
+        def forward(self, input):
+                return input.view(*self.shape)
+
+class Net(nn.Module):
+        def __init__(self, dims,config):
+                super(Net, self).__init__()
+                self.modules = []
+                nchannels = dims[0]
+
+                self.func = F.relu
+                self.dropout = nn.Dropout(p=config.dropout)
+
+                avgpool1 = nn.AvgPool3d(2,stride=2)
+                self.add_module('avgpool_0', avgpool1)
+                self.modules.append(avgpool1)
+                conv1 = nn.Conv3d(nchannels,out_channels=32,padding=1,kernel_size=3,stride=1) 
+                self.add_module('unit1_conv',conv1)
+                self.modules.append(conv1)
+                conv2 = nn.Conv3d(32,out_channels=32,padding=0,kernel_size=1,stride=1) 
+                self.add_module('unit2_conv',conv2)
+                self.modules.append(conv2)
+                avgpool2 = nn.AvgPool3d(2,stride=2)
+                self.add_module('avgpool_1', avgpool2)
+                self.modules.append(avgpool2)
+                conv3 = nn.Conv3d(32,out_channels=64,padding=1,kernel_size=3,stride=1) 
+                self.add_module('unit3_conv',conv3)
+                self.modules.append(conv3)
+                conv4 = nn.Conv3d(64,out_channels=64,padding=0,kernel_size=1,stride=1) 
+                self.add_module('unit4_conv',conv4)
+                self.modules.append(conv4)
+                avgpool3 = nn.AvgPool3d(2,stride=2)
+                self.add_module('avgpool_2', avgpool3)
+                self.modules.append(avgpool3)
+                conv5 = nn.Conv3d(64,out_channels=128,padding=1,kernel_size=3,stride=1) 
+                self.add_module('unit5_conv',conv5)
+                self.modules.append(conv5)
+                div = 2*2*2
+                last_size = int(dims[1]//div * dims[2]//div * dims[3]//div * 128)
+                flattener = View((-1,last_size))
+                self.add_module('flatten',flattener)
+                self.modules.append(flattener)
+                self.pose_output = nn.Linear(last_size,2)
+                self.affinity_output = nn.Linear(last_size,1)
+                self.softmax = nn.Softmax(dim = 1)
+
+        def forward_one(self, x): #should approximate the affinity of the receptor/ligand pair
+                for layer in self.modules:
+                        if not isinstance(layer, nn.Linear):
+                                x= layer(x)
+                                if isinstance(layer,nn.Conv3d):
+                                        x=self.func(x)
+                return x
+
+        def forward(self,x1):
+                lig1 = self.forward_one(x1)
+                original_pose = self.pose_output(lig1)
+                pose_softmax = self.softmax(original_pose)
+                pose = torch.Tensor(pose_softmax)[:,1]
+                affinity = self.affinity_output(lig1)
+                
+                return (lig1, pose, affinity, original_pose)
